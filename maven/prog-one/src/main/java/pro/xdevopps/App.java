3878f44dc5b2d9c168000c677cc4ebd8e8e5cb05
mmi16389 <yvon.michel.ext@capgemini.com>
2021-05-06 09:33:40 +0200

    Init java project

---
+++ maven/prog-one/src/main/java/pro/xdevopps/App.java
@@ -0,0 +1,7 @@
+package pro.xdevopps;
+
+public class App {
+    public static  void main(String ... args){
+
+    }
+}
---
+++ maven/prog-two/src/test/java/pro/xdevopps/AppTest.java
@@ -0,0 +1,12 @@
+package pro.xdevopps;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void Test1(){
+        assertTrue(true);
+    }
+}
